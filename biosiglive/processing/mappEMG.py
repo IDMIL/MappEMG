a0de99b27618605508ecf1e1a85b943b51a9e87a
Noa Kemp <noakemp@Noas-MBP.lan>
2022-06-09 15:31:27 -0400

    mappEMG added functions

--- biosiglive/processing/mappEMG.py
+++ biosiglive/processing/mappEMG.py
@@ -24,6 +24,10 @@ class EMGprocess:
 
     # function to update currently passed emg value, as well as updating the previous values array
     def input(self, x): 
+        '''
+        function that takes emg input (in %MVC)
+        updates x_emg attribute of object
+        '''
         self.x_emg = float(x)
         if self.input_started == False: # if this is the first input passed, then the smoothed input is the same as the first input
             self.x_emg_smoothed = float(x) 
@@ -32,7 +36,15 @@ class EMGprocess:
         # if len(self.prev_values) > 500:
         #     self.prev_values = self.prev_values[len(self.prev_values)-100:len(self.prev_values)] # updating prev values to only be the last 100 values
 
-
+    def clip(self):
+        '''
+        clips %MVC input such that it stays between 0 and 1
+        '''
+        if self.x_emg > 1:
+            self.x_emg = 1
+        if self.x_emg < 0:
+            self.emg = 0
+        
     # @staticmethod
     # def local_maxima(array):
     #     try:
@@ -40,7 +52,6 @@ class EMGprocess:
     #     except ValueError:  # should happen if inputed array is less than 100 values
     #         return np.amax(array)
     
-
     def slide(self, slide_up = 1/5, slide_down = 1/5):
         if self.x_emg > self.x_emg_smoothed: # if cur x value is bigger than prev computed smoothed val
             self.x_emg_smoothed = self.x_emg_smoothed + (self.x_emg - self.x_emg_smoothed) / slide_up
@@ -70,8 +81,14 @@ class Mapper:
         self.n = n
         self.inputs = dict()
         for i in range(1,n+1):
-            self.inputs[i] = 0
-        self.weighted_emgs = 0
+            self.inputs[i] = 0.0
+        self.weighted_emgs = 0.0
+        self.freqthreshold = 0.0
+        self.amplthreshold = 0.0
+        self.yminf = 0.0
+        self.ymina = 0.0
+        self.ymaxf = 127.0
+        self.ymaxa = 127.0
     
     
     def input(self, x, n):
@@ -123,43 +140,58 @@ class Mapper:
         elif mapping == 'a':
             self.amplitude_points.sort(key=lambda tup: tup[0])  # sorting the list according to x values
 
+
+    def addThreshold(self, threshold, mapping):
+        '''
+        updates threshold values used in mapper
+
+        Arguments:
+        ---
+        threshold: threshold value
+        mapping: 'f' or 'a', 'f' = frequency mapper & 'a' = amplitude mapper
+        '''
+        if mapping == 'f':
+            self.freqthreshold = threshold
+        if mapping == 'a':
+            self.amplthreshold = threshold
     
-    
+
+    def changeMinY(self, y, mapping):
+        '''
+        updates minimal y value in the mapping
+
+        Arguments:
+        ---
+        y: y value
+        mapping: 'f' or 'a', 'f' = frequency mapper & 'a' = amplitude mapper
+        '''
+        if mapping == 'f':
+            self.yminf = y
+        if mapping == 'a':
+            self.ymina = y
+
+        
     def mapper(self, x, mapping, **extra):
         '''
-        Make sure x input from class is from 0 to 1, else change offset
+        given a list of points in a function, outputs y value of x 
 
         Arguments
-        ------
-        x: value we want a mapping for
+        ---
+        x: value we want a mapping for (should be from 0 to 1)
         mapping: 'f' or 'a', 'f' = frequency mapper & 'a' = amplitude mapper
-        Kwargs in extra:
-            threshold: before this value, return min value
-            ymin: FLOAT, default is 0
-            ymax: FLOAT, default is 127
-        
         '''
-        ymin = 0.0
-        ymax = 127.0
 
         if mapping == 'f':
             points = self.frequency_points
+            threshold = self.freqthreshold
+            ymin = self.yminf
         elif mapping == 'a':
             points = self.amplitude_points
-
-        if 'ymax' in extra:  # updating ymax if passed as kwarg
-            ymax = float(extra['ymax'])
-        if 'ymin' in extra:  # updating min if passed as kwarg
-            ymin = float(extra['ymin'])
-        if 'threshold' in extra:  # if there is a threshold in kwargs
-            if x <= float(extra['threshold']):  # if x is less than threshold
-                return ymin
-
-        # # safety in case new max is passed and not passed as a new point , im not sure if it's the best idea
-        # if ymax > max(points, key=lambda item: item[1])[1]:  # if ymax passed is bigger than cur max
-        #     points.pop(len(points) - 1)  # pop last element
-        #     points.append((1, ymax))  # replace with new max
-
+            threshold = self.amplthreshold
+            ymin = self.ymina
+        
+        if x < threshold:  # if x is less than threshold
+            return ymin
 
         prev_point = points[0]  # getting first point
         for point in points[1:]:
@@ -175,6 +207,9 @@ class Mapper:
 
     
     def toFreqAmpl(self,x):
+        '''
+        takes x and outputs its mapping as a tuple of the form (amplitude,frequency)
+        '''
         f = self.mapper(x,'f')
         a = self.mapper(x,'a')
         return (a,f)
