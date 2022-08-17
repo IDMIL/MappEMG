6279457b4f70a352517bda6d39e6db5b48b8692f
Noa Kemp <noakemp@Noas-MBP.lan>
2022-08-17 15:00:43 -0400

    renamed run_me to installation.sh and added windows option

--- installation.sh
+++ installation.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
-foo=0
 read -p "Do you already have a virtual environment you would like to use (y/n)? If you do not know what is a virtual environment, simply press 'n' " yn
 ##### if yes, then just activate the venv and install the reqs #####
+
 if [[ "$yn" == "y" ]]; then 
     read -p "What's the name of the virtual environment? " venv
-    foo=1   
+
+elif [[ "$yn" == "n" ]]; then
+    pip3 install virtualenv
+    virtualenv mappemg_venv
+    venv="mappemg_venv"
+    
 fi
 
 pip install --upgrade pip #upgrading pip
 
 if [[ "$OSTYPE" == "darwin"* ]]; then # Mac OSX
-        if [[ "$foo" -eq 0 ]]; then
-            pip3 install virtualenv
-            virtualenv mappemg_venv
-            venv="mappemg_venv"
-        fi
-        source $venv/bin/activate
-        echo $VIRTUAL_ENV
-        echo "--------------------"
-        echo "--------------------"
-        #install reqs
+    source $venv/bin/activate
 
-fi
+elif [[ "$OSTYPE" == "msys" ]]; then
+    # Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
+    source $venv/Scripts/activate
+
+fi     
+
 pip install -r requirements.txt
 
 python setup.py install
