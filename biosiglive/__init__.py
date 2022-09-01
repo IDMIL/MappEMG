b30ab2f48f6b31676f9e6ae884100617574e488b
Karl Michel Koerich <karlmiko@gmail.com>
2022-09-01 14:54:38 -0400

    Removed imports from biosiglive init

--- biosiglive/__init__.py
+++ biosiglive/__init__.py
@@ -3,13 +3,9 @@ from .gui.plot import Plot, LivePlot
 from .interfaces.pytrigno_interface import PytrignoClient
 from .interfaces.vicon_interface import ViconClient
 from .interfaces.client_interface import TcpClient
-from .interfaces.param import Type, Device, MarkerSet
-
-
-from .io.save_data import read_data, add_data_to_pickle
+from .interfaces.param import Type, Device
 
 from .processing.data_processing import RealTimeProcessing, OfflineProcessing, GenericProcessing
-from .processing.msk_functions import kalman_func
 
 from .streaming.client import Client
 from .streaming.connection import Server
